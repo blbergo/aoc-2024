1bcdf66c04539ad6a5b2dbb9b6067618300c0298
blbergo <bryan.l.bergo@gmail.com>
2024-11-30 21:52:15 -0800

    gitignore

--- day1/obj/Debug/net9.0/day1.AssemblyInfo.cs
+++ day1/obj/Debug/net9.0/day1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("day1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6d7c90b322d38b3d208963ef771495eda715da6d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5dfdbc437d2884893d9658c280208dbc9dba3586")]
 [assembly: System.Reflection.AssemblyProductAttribute("day1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("day1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
